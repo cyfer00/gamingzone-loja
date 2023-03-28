f3f7c76b8ce13348ebcb6f78cd23ea8614efc0d5
cyfer00 <gabriel.glins2002@gmail.com>
2023-03-28 03:43:21 -0300

    recuperacaoUC10

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user'
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  userModel = new User("", "")
+  mensagem = ""
 
+  constructor(private loginService: LoginService) { }
+
+  receberDados(){
+    console.log(this.userModel)
+
+    this.loginService.login(this.userModel).subscribe((response) =>  {
+      console.log("response:", response)
+      console.log("O Status Code é ", response.status)
+      console.log("O Token de permissão é:", response.body.accessToken)
+
+
+      this.mensagem = "Bem vindo " + response.body.user.nome
+    }, (erro) => {
+      console.log(erro)
+      this.mensagem= "Falha ao efetuar o login, tente novamente mais tarde "
+    })
+  }
 }
